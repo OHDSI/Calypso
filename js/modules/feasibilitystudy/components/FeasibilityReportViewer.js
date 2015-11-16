2558891054f625039c78a20a57804527e748dd3b
Chris Knoll <cknoll@ohdsi.org>
2015-11-15 23:58:52 -0500

    Added attrition report.
    Re-factored current visualization into intersection report'
    Added toggle between different views.

--- js/modules/feasibilitystudy/components/FeasibilityReportViewer.js
+++ js/modules/feasibilitystudy/components/FeasibilityReportViewer.js
@@ -1,72 +1,18 @@
 define(['knockout',
 				'jquery',
 				'text!./FeasibilityReportViewerTemplate.html',
-				'../bindings/populationTreemapBinding',
-				'databindings/eventListenerBinding',
+				'./FeasibilityIntersectReport',
+				'./FeasibilityAttritionReport',
 			  'css!../css/report.css'],
 	function (
 		ko,
 		$,
 		template) {
 
-
-		function bitCounter(bits) {
-			counted = 0;
-			for (b = 0; b < bits.length; b++) {
-				if (bits[b] == '1') {
-					counted++;
-				}
-			}
-			return counted;
-		}
-
 		function FeasibilityReportViewer(params) {
 			var self = this;
 			self.report = params.report;
-			self.rectSummary = ko.observable();
-			self.pass = ko.observableArray();
-			self.fail = ko.observableArray();
-			
-			self.describeClear = function() {
-				self.rectSummary(null);
-				self.pass.removeAll();
-				self.fail.removeAll();
-			}
-
-			self.describe = function(bits, size) {
-				var matched = bitCounter(bits);
-				var pass_count = 0;
-				var fail_count = 0;
-				var passed = [];
-				var failed = [];
-				
-				for (b = 0; b < bits.length; b++) {
-					if (bits[b] == '1') {
-						passed.push(self.report().inclusionRuleStats[b]);
-						pass_count++;
-					} else {
-						failed.push(self.report().inclusionRuleStats[b]);
-						fail_count++;
-					}
-				}
-
-				var percentage = 0;
-				if (self.report().summary.totalPersons > 0) {
-					percentage = (size / self.report().summary.totalPersons * 100);
-				}
-				self.pass(passed);
-				self.fail(failed);
-				self.rectSummary(size + ' people (' + percentage.toFixed(2) + '%), ' + pass_count + ' criteria passed, ' + fail_count + ' criteria failed.');
-			}
-			
-			self.handleCellOver = function(data, event) {
-				if (event.target.__data__) {
-					var treemapDatum = event.target.__data__;
-					self.describe(treemapDatum.name, treemapDatum.size);
-				} else {
-					return false;
-				}
-			}			
+			self.selectedView = ko.observable("intersect");
 
 			if (params.viewerWidget) {
 				viewerWidget(self);
